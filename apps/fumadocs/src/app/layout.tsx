da42faf3818440adb3a276c070e125730fd6736c
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-11-26 10:25:12 +0100

    feat: add metadata for layout component
    - Introduced metadata for the layout component, including a title and description to enhance SEO and provide context for users about Vercord's functionality.

--- apps/fumadocs/src/app/layout.tsx
+++ apps/fumadocs/src/app/layout.tsx
@@ -1,10 +1,17 @@
 import { RootProvider } from "fumadocs-ui/provider/next";
+import type { Metadata } from "next";
 import "./global.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: "vercord",
+  description:
+    "Seamlessly integrate your Vercel deployment notifications with Discord.",
+};
 
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
